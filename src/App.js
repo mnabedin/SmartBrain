263feba8b3f04d8cb734bf5a5c06188e94806959
mnabedin <mnabedin@uwaterloo.ca>
2020-07-22 22:28:45 -0400

    added face recognition box

--- src/App.js
+++ src/App.js
@@ -3,17 +3,24 @@ import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition'
 import Particles from 'react-particles-js';
+import Clarifai from 'clarifai';
 import './App.css';
+
+
+const app = new Clarifai.App({
+  apiKey: '322c4f6bc6e24bfb96a904c3d6e3e589'
+ });
 
 
 const particlesOptions = {
   particles: {
     number: {
-      value: 40,
+      value: 45,
       density: {
         enable: true,
-        value_area: 350
+        value_area: 325
       }
     },
     line_linked: {
@@ -28,6 +35,48 @@ const particlesOptions = {
 
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+      imageUrl: '',
+      box: {},
+    }
+  }
+
+
+  calculateFaceLocation = (data) => {
+    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById('inputimage');
+    const width = Number(image.width);
+    const height = Number(image.height);
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - (clarifaiFace.right_col * width),
+      bottomRow: height - (clarifaiFace.bottom_row * height)
+    }
+  }
+
+
+  displayFaceBox = (box) => {
+    this.setState({box: box});
+  }
+
+
+  onInputChange = (event) => {
+    this.setState({input: event.target.value});
+  }
+
+
+  onButtonSubmit = () => {
+    this.setState({imageUrl: this.state.input});
+    app.models.predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
+    .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
+    .catch(err => console.log(err));
+  }
+
+
   render() {
     return (
       <div className="App">
@@ -35,9 +84,8 @@ class App extends Component {
         <Navigation />
         <Logo />
         <Rank />
-        <ImageLinkForm />
-        {/*
-        <FaceRecognition />} */}
+        <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit}/>
+        <FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl}/>
       </div>
     );
   }
