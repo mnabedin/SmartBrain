5b99d19b54e27f4ee7b17fcdda73bdf97d7ec517
Nafis Abedin <54211211+mnabedin@users.noreply.github.com>
2020-10-05 14:15:13 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
-import FaceRecognition from './components/FaceRecognition/FaceRecognition'
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import Signin from './components/Signin/Signin';
 import Register from './components/Register/Register';
 import Particles from 'react-particles-js';
